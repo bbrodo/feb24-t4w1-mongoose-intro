a4d3795e25a787589fdfeb27fa7a0f8b46be2d60
bbrodo <15292@coderacademy.edu.au>
2024-11-06 13:58:04 +1100

    feat: dbconnect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { app } = require("./server.js");
+const { dbConnect } = require("./utils/database.js");
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await dbConnect();
+
     console.log("Server is running on port " + PORT);
 });
