8ce048ea372cc3035bdf6546d4795da8a8cda7b4
bbrodo <15292@coderacademy.edu.au>
2024-11-10 14:44:29 +1100

    feat: seeding languagecodes

--- src/utils/crud/seeding.js
+++ src/utils/crud/seeding.js
@@ -2,12 +2,16 @@ const { dbConnect, dbDisconnect } = require("../database");
 const { createPost } = require("./PostCrud");
 const { createUser } = require("./UserCrud");
 
-
-
 async function seed() {
-    let newUser = await createUser("brodey", true, "brodey880@gmail.com")
-    await createPost("Example title", "Example Content", newUser.id);
-    console.log("Seeding completed, disconnecting")
+    let newUser = await createUser("brodey", true, "brodey880@gmail.com");
+    await createPost(
+        "Example title",
+        [{ languageCode: "en", content: "example content" },
+            {languageCode: "fr", content: "omelete baugette"}
+        ],
+        newUser.id
+    );
+    console.log("Seeding completed, disconnecting");
     await dbDisconnect();
 }
 
